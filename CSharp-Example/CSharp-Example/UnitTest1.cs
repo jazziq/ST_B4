82f55ad2c241b15950355f0d4f4cb63726c2858f
JAZZID <JAZZID@JAZZID>
2019-04-09 22:54:15 +0200

    add task_01 add task_03

---
+++ CSharp-Example/CSharp-Example/UnitTest1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Remote;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Support.Events;
+
+
+namespace CSharp_Example
+{
+  [TestFixture]
+  public class MyFirstTest
+  {
+    private IWebDriver driver;
+    private WebDriverWait wait;
+    private string baseURL;
+
+    [SetUp]
+    public void Start()
+    {
+      InternetExplorerOptions options = new InternetExplorerOptions();
+      options.IntroduceInstabilityByIgnoringProtectedModeSettings = true;
+      driver = new InternetExplorerDriver(options);
+      wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+    }
+
+
+    [Test]
+    public void TestMethod1()
+    {
+      baseURL = "https://www.google.com";
+      driver.Navigate().GoToUrl(baseURL);
+      driver.FindElement(By.Name("q")).Click();
+      driver.FindElement(By.Name("q")).Clear();
+      driver.FindElement(By.Name("q")).SendKeys("тест");
+      driver.FindElement(By.Name("btnK")).Click();
+      wait.Until(ExpectedConditions.TitleIs("тест - Поиск в Google"));
+    }
+
+    [TearDown]
+    public void Stop()
+    {
+      driver.Quit();
+      driver = null;
+    }
+  }
+}
---
+++ litecart_project/litecart_project/UnitTest1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Remote;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Support.Events;
+
+
+namespace litecart_project
+{
+  [TestFixture]
+  public class litecart_project_tests
+  {
+    private IWebDriver driver;
+    private WebDriverWait wait;
+    private string baseURL;
+
+    [SetUp]
+    public void Start()
+    {
+      InternetExplorerOptions options = new InternetExplorerOptions();
+      options.IntroduceInstabilityByIgnoringProtectedModeSettings = true;
+      driver = new InternetExplorerDriver(options);
+      wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+      baseURL = "http://localhost";
+    }
+
+
+    [Test]
+    public void LoginToLitecart()
+    {
+      driver.Navigate().GoToUrl(baseURL + "/litecart/admin/login.php");
+      driver.FindElement(By.Name("username")).Clear();
+      driver.FindElement(By.Name("username")).SendKeys("admin");
+      driver.FindElement(By.Name("password")).Clear();
+      driver.FindElement(By.Name("password")).SendKeys("admin");
+      driver.FindElement(By.Name("login")).Click();
+
+      wait.Until(ExpectedConditions.TitleIs("My Store"));
+    }
+
+    [TearDown]
+    public void Stop()
+    {
+      driver.Quit();
+      driver = null;
+    }
+  }
+}
